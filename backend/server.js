ad5c9915c8b1d367084129d83314798480592df9
Ofek Yemini <ofekyemini87@gmail.com>
2025-09-04 14:40:46 +0300

    fix backend server.js

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,6 @@ app.use(cors());
 app.use(express.json());  
 app.use("/api/tasks", taskRoutes); 
 
-// Check if the server is working correctly 
-app.get("/api/health", (req, res) => res.json({ ok: true })); 
-
 // Error handler:
 app.use(errorHandler);
 
